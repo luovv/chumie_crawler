a5da0914f97bcc2cee889cc0a618cb277c19c808
vv <vv@pc-141-167.customer.courtrooms.knightsbridge.lan>
2017-03-04 00:20:40 +0000

    firstcommit

---
+++ crawler/pipelines.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+# from scrapy.contrib.exporter import JsonItemExporter
+from pymongo import MongoClient
+
+class JsonWriterPipeline(object):
+
+    def open_spider(self, spider):
+        self.db = MongoClient('mongodb://35.161.12.150/myapp', 27017).myapp.webcontents
+
+    def close_spider(self, spider):
+        pass
+        # 默认是unicode，ensure_ascii=False存中文
+        # indent=4就是带缩进的，默认是不换行的
+        # line = json.dumps(self.itemList, ensure_ascii=False, indent=4) + "\n"
+        #
+        # file = open('items.json', 'w', encoding='utf-8')
+        # file.write(line)
+        # file.close()
+
+    def process_item(self, item, spider):
+        self.db.insert_one(item)
+        return item
---
+++ crawler/spiders/dota2_sgamer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import datetime
+import scrapy
+import re
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "dota2_sgamer"
+    # 网站入口
+    start_urls = [
+        'http://dota2.sgamer.com/',
+    ]
+    # 通过列表页采集详情页url
+    def parse(self, response):
+        for i in response.xpath('//li[@class="index-new"]'):
+            url = i.xpath('div[@class="news-list"]/h2[@class="tit"]/a/@href').extract_first()
+            image = i.xpath('a[@class="img"]/img/@src').extract_first()
+            print(url)
+            # 有的url是相对路径，改成绝对路径
+            if not 'http' in url:
+                url = 'http://dota2.sgamer.com'+url
+            # video不采
+
+            if not 'video' in url:
+                # 请求详情页callback到parse_detail
+                request = scrapy.Request(url, callback=self.parse_detail)
+                request.meta['url'] = url
+                request.meta['image'] = image
+                yield request
+
+    def parse_detail(self, response):
+        url = response.meta['url']
+        image = response.meta['image']
+        content=response.xpath('//div[@class="text"]').extract_first()
+        dr = re.compile(r'<[^>]+>', re.S)
+        content = dr.sub('', content)
+        content = content.replace('\n', '')
+        content = content.replace('\t', '')
+        content = content.replace('\r', '')
+        yield {
+            'title': response.xpath('//h1/text()').extract_first(),
+            'url': url,
+            'image': image,
+            'content': content,
+            'crawledDate:': datetime.datetime.now(),
+            'tag': ['dota2'],
+            'imgRatio':1
+        }
+
